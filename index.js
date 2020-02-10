f5871995b497710281d2b7cd8db8cc69060fb123
CaleeebC <33111047+CaleeebC@users.noreply.github.com>
2020-02-10 08:04:17 -0800

    Removed key

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const fs = require('fs');
 const options = {
     uri: 'https://www.thebluealliance.com/api/v3/match/2019cc_f1m1/zebra_motionworks',
     qs: {
-        access_token: '461j8WDWWysm5UgVJkwtLvefN223ZPFChDFiQUpkmeGCBM4pYVCChJqUTCiUjZtM' // -> uri + '?access_token=xxxxx%20xxxxx'
+        access_token: 'XXXXX' // -> uri + '?access_token=xxxxx%20xxxxx'
     },
     headers: {
         'User-Agent': 'Request-Promise',
-        'X-TBA-Auth-Key' : '461j8WDWWysm5UgVJkwtLvefN223ZPFChDFiQUpkmeGCBM4pYVCChJqUTCiUjZtM'
+        'X-TBA-Auth-Key' : 'XXXX'
     },
     json: true // Automatically parses the JSON string in the response
 };
